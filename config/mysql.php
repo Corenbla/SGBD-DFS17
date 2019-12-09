845d2d8be3fa577d4961e07ab8bec195b7c8c6d3
c.richard-vitton <c.richard-vitton@it-students.fr>
2019-12-09 16:00:21 +0100

    Homepage

---
+++ config/mysql.php
@@ -0,0 +1,6 @@
+<?php
+
+// local MySQL config, change to whatever you're using.
+$user = 'user';
+$password = '1234';
+$database = 'pokeshop';
---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pokéshop</title>
+    <!-- jQuery / Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<?php require_once 'templates/navbar.php'?>
+<h1 class="jumbotron container">My awesome website</h1>
+
+
+
+
+</body>
+</html>
